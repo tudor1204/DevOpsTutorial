03697a9884598b10c83404be7925965b23f77730
vlad.vasile <vlad.vasile@siemens.com>
2023-08-30 07:04:17 +0300

    Small text change

--- main/main.cpp
+++ main/main.cpp
@@ -5,8 +5,8 @@
 int main()
 {
 	Rectangle r(10,5);
-	std::cout<<"Rectangle permiterer: " << r.calculatePermieter() << std::endl;
-	std::cout<<"Rectangle area: " << r.calculateArea() << std::endl;
+	std::cout<<"Rectangle permiterer is: " << r.calculatePermieter() << std::endl;
+	std::cout<<"Rectangle area is: " << r.calculateArea() << std::endl;
 	
 	return 1;
 }
